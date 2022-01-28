992aa44b59cdaa1e954fb3a8035ef9f105937035
LeoLeLion1 <leo.gizard@outlook.fr>
2022-01-28 23:31:07 +0100

    fixing bug

--- GGJ-project/Assets/Scripts/Interruptor.cs
+++ GGJ-project/Assets/Scripts/Interruptor.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Interruptor : MonoBehaviour
 {
 
+    // the game manager who receive message
+    //  [SerializeField] GameManager gm;
+
+    // number of player on the Interruptor
+    private int onInterruptor;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        onInterruptor = 0;  
     }
 
     // Update is called once per frame
@@ -21,16 +27,20 @@ public class Interruptor : MonoBehaviour
     {
         
         if ( col.tag == "player") {
+            onInterruptor ++;
             // TODO send a message to the game manager
-            Debug.Log("Player on button");
+            Debug.Log("Player(s) on interruptor");
         }
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
         if ( col.tag == "player") {
-            // TODO send a message to the game manager
-            Debug.Log("Player not on button");
+            onInterruptor--;
+            if ( onInterruptor == 0) {
+                // TODO send a message to the game manager
+                Debug.Log("No player on interruptor");
+            }
         }
 
     }
