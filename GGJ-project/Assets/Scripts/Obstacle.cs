8845cdd0d5eeda271290e1d47e5682e892e3a1bf
BiscuitPrime <henri.nomico@gmail.com>
2022-01-29 20:21:25 +0100

    LevelOne Tab 2 added

--- GGJ-project/Assets/Scripts/Obstacle.cs
+++ GGJ-project/Assets/Scripts/Obstacle.cs
@@ -6,20 +6,6 @@ public class Obstacle : MonoBehaviour
 {
 
     [SerializeField] private GameManager gm;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
