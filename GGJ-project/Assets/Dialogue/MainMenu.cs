14c78e50d74e276cb67411e2f059d40c4f3517ad
BowRaiders <53614982+BowRaiders@users.noreply.github.com>
2022-01-30 14:07:20 +0100

    Added dialogues

--- GGJ-project/Assets/Dialogue/MainMenu.cs
+++ GGJ-project/Assets/Dialogue/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -27,6 +28,7 @@ public class MainMenu : MonoBehaviour
     public void OpenCredits()
     {
         // open credit scene
+        SceneManager.LoadScene("Credits");
     }
 
     public void QuitGame()
