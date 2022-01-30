04fb081d24e780e1d61782996968575afc65a7bd
GuillaumeCx <guillaume.caucheteux@hotmail.fr>
2022-01-30 15:28:15 +0100

    some corrections

--- GGJ-project/Assets/Dialogue/TextWriter.cs
+++ GGJ-project/Assets/Dialogue/TextWriter.cs
@@ -100,10 +100,8 @@ public class TextWriter : MonoBehaviour
             {
                 text += "<color=#00000000>" + sentence.Substring(index) + "</color>";
             }
-            if(sentence[index]  != ' ')
-            {
-                sound.PlaySound();
-            }
+            
+            sound.PlaySound();
             txt.text = text;
             index++;
         }
